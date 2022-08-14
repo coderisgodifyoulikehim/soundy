85cb804b22ef9850eeac4d1d534240a307c88e65
coderisgodifyoulikehim <97345381+coderisgodifyoulikehim@users.noreply.github.com>
2022-08-14 10:52:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function preload() {
+    song = loadSound("music.mp3");
+    }
+    
+    function setup() {
+        canvas = createCanvas(600, 500);
+        canvas.center();
+
+        video = createCapture(VIDEO);
+        video.hide();
+    }
